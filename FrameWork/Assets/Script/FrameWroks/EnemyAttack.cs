1d5aa7c2ffc6ce0c004e61c0bf48ef2a6ea13e96
Jacob Jellison <Jellymanj99@gmail.com>
2017-08-03 21:52:30 -0700

    AI/

---
+++ FrameWork/Assets/Script/FrameWroks/EnemyAttack.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange = false;
+    float timer;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();
+        anim.SetBool("Walking", true);
+    }
+
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+            anim.SetBool("Attacking", false);
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        
+
+        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        {
+            Attack ();
+        }
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetBool("PlayerDead", false);
+        }
+        
+
+       
+
+        
+    }
+
+
+    void Attack ()
+    {
+        timer = 0f;
+
+
+        if(playerHealth.currentHealth > 0)
+        {
+            anim.SetBool("Attacking", true);
+            anim.SetBool("PlayerDead", false);
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
