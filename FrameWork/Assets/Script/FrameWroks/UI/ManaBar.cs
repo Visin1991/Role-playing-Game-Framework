04eb6f1bfa9fdcd81ecfe86fc6c440a6bb0aedd1
Jacob Jellison <Jellymanj99@gmail.com>
2017-08-10 20:50:46 -0700

    Add files via upload

---
+++ FrameWork/Assets/Script/FrameWroks/UI/ManaBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+
+    Image manaImage;
+
+    private void Start()
+    {
+        manaImage = GetComponent<Image>();
+        GameCentalPr.Instance.Adapter_Manabar -= ValueChange;
+        GameCentalPr.Instance.Adapter_Manabar += ValueChange;
+    }
+
+    public void ValueChange(float current, float max)
+    {
+        if (manaImage != null)
+        {
+            manaImage.fillAmount = current / max;
+        }
+    }
+}
