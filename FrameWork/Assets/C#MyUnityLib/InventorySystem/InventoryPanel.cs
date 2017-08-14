4e48f0e472e89795ce41814ccea97fd270811aaa
Visin___Wei <zhuzhanhao1991@gmail.com>
2017-08-14 09:23:36 -0700

    Add Inventory System

---
+++ FrameWork/Assets/C#MyUnityLib/InventorySystem/InventoryPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class InventoryPanel : MonoBehaviour {
+
+    ItemHandle[] allHandles;
+
+    [SerializeField]
+    private ItemDataBaseList itemDatabase;
+
+    // Use this for initialization
+    void Start () {
+        allHandles = GetComponentsInChildren<ItemHandle>();
+        List<Item> items = new List<Item>();
+
+        for (int i = 0; i < 20; i++)
+        {
+            items.Add(itemDatabase.getItemCopyByID(i % 2));
+        }
+
+        ReSetItems(items);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            AddItem(itemDatabase.getItemCopyByID(2));
+        }
+    }
+
+    public void ReSetItems(List<Item> items)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            allHandles[i].ResetItem(items[i]);
+        }
+    }
+
+    public void AddItem(Item item)
+    {
+        allHandles.First(h => h.IsEmpty).ResetItem(item); 
+    }
+}
---
+++ FrameWork/Assets/C#MyUnityLib/InventorySystem/ItemHandle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHandle : MonoBehaviour {
+
+    public GameObject itemImageObj;
+    public Image itemImage;
+
+    public Item item;
+
+    [SerializeField]
+    bool isEmpty = true;
+
+    public void Start()
+    {
+        itemImage = GetComponentInChildren<Image>();
+        itemImageObj = itemImage.gameObject;
+        if (isEmpty)
+        {
+            itemImageObj.SetActive(false);
+        }
+    }
+
+    public bool IsEmpty
+    {
+        get { return isEmpty; }
+    }
+
+    public void ResetItem(Item _item)
+    {
+        item = _item;
+        if (item != null)
+        {
+            isEmpty = false;
+            if(itemImageObj!=null)
+                itemImageObj.SetActive(true);
+            if(itemImage!= null)
+                itemImage.sprite = item.itemIcon;
+        }
+    }
+
+    public void Clean()
+    {
+        item = null;
+        if(itemImageObj)
+            itemImageObj.SetActive(false);
+        isEmpty = true;
+    }
+
+}
